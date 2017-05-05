970d3089d495863fe802af2ed9422d46b2fe8183
Lauriezip <lauriezip@gmail.com>
2017-05-05 17:55:11 -0400

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name, devoured) VALUES ('cheese burger',true);
+INSERT INTO burgers (name, devoured) VALUES ('double cheese burger',false);
+INSERT INTO burgers (name, devoured) VALUES ('bacon cheese burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('veggie burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('veggie cheese burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('double veggie burger',true);
